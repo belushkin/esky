b619bcef7f99394993251457e9e6d778a0955fda
Maxim Bielushkin <belushkin@gmail.com>
2014-12-16 22:49:47 +0200

    Added new test

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-
-$autoload = new Composer\Autoload\ClassLoader();
-$autoload->add('OHT', FWPATH);
-$autoload->add('', FWPATH . 'Controllers');
-$autoload->register();
